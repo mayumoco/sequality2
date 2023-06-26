cd02fa6d9f2b17786f91a73ed5c5f9dbce23e24c
m1m23a34 <m1m23a34@oit.ac.jp>
2023-06-27 00:20:20 +0900

    XXXX

--- app/src/test/java/sequality/AppTest.java
+++ app/src/test/java/sequality/AppTest.java
@@ -10,7 +10,7 @@ public class AppTest {
   @Test
   public void testAppReternHelloWorld() {
     App classUnderTest = new App();
-    assertEquals("Hello World!", classUnderTest.getGreeting());
+    assertEquals("Hello World", classUnderTest.getGreeting());
   }
 
 }
