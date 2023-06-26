3a2f3feec4672a5a947cbe615ce94b536c525216
m1m23a34 <m1m23a34@oit.ac.jp>
2023-06-26 23:33:07 +0900

    XXXX

--- app/src/main/java/sequality/Calculate.java
+++ app/src/main/java/sequality/Calculate.java
@@ -4,4 +4,41 @@ public class Calculate {
   public int sum(int x, int y) {
     return x + y;
   }
+
+  public double average(int x, int y) {
+    return (double) (x + y) / 2;
+  }
+
+  public int sum(int[] numbers) {
+    int sum = 0;
+    for (int num : numbers) {
+      sum += num;
+    }
+    return sum;
+  }
+
+  public double average(int[] numbers) {
+    int sum = sum(numbers);
+    return (double) sum / numbers.length;
+  }
+
+  public int addOddNumbers(int[] numbers) {
+    int sum = 0;
+    for (int num : numbers) {
+      if (num % 2 != 0) {
+        sum += num;
+      }
+    }
+    return sum;
+  }
+
+  public int addEvenNumbers(int[] numbers) {
+    int sum = 0;
+    for (int num : numbers) {
+      if (num % 2 == 0) {
+        sum += num;
+      }
+    }
+    return sum;
+  }
 }
